849bd4fc4fe910b79c03dc6b39eee105b3a7ea21
Torben Johnson <tjohnson@dimonoff.com>
2025-12-23 08:57:00 -0500

    build(nix): improve flake systems and shells

--- flake.nix
+++ flake.nix
@@ -16,41 +16,50 @@
       rust-overlay,
     }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ rust-overlay.overlays.default ];
-        config.allowUnfree = true;
-      };
-      rustToolchain = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
-      devRustToolchain = rustToolchain.override {
-        extensions = [ "rust-src" ];
-      };
+      inherit (nixpkgs.lib) genAttrs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
     in
     {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          nativeBuildInputs = [
-            devRustToolchain
-            pkgs.rust-analyzer
-            pkgs.cargo-nextest
-            pkgs.just
-            pkgs.tombi
-            pkgs.wild
-            pkgs.clang
-          ];
-        };
+      devShells = genAttrs systems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+            config.allowUnfree = true;
+          };
+          rustToolchain = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
+          devRustToolchain = rustToolchain.override {
+            extensions = [ "rust-src" ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              devRustToolchain
+              pkgs.rust-analyzer
+              pkgs.cargo-nextest
+              pkgs.just
+              pkgs.tombi
+              pkgs.wild
+              pkgs.clang
+            ];
+          };
 
-        ci = pkgs.mkShell {
-          nativeBuildInputs = [
-            rustToolchain
-            pkgs.cargo-nextest
-            pkgs.just
-            pkgs.tombi
-            pkgs.wild
-            pkgs.clang
-          ];
-        };
-      };
+          ci = pkgs.mkShell {
+            nativeBuildInputs = [
+              rustToolchain
+              pkgs.cargo-nextest
+              pkgs.just
+              pkgs.tombi
+              pkgs.wild
+              pkgs.clang
+            ];
+          };
+        }
+      );
     };
 }
